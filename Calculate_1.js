870425bf63afafc9d50f77a2a667681741330d4a
Erza0838 <muhmmaderza884@gmail.com>
2023-02-01 06:13:05 +0700

    Memperbaiki kalkulator hari ke-8

--- Calculate_1.js
+++ Calculate_1.js
@@ -12,27 +12,26 @@ let displayIdOfNumberAndOperator = document.getElementById("Display_Output")
 // Variable untuk menampilkan hasil perhitungan 
 let showFinalResult = document.getElementById("Display_Result")
 
-// Variable untuk mengakses DOM 
-let getDomElement = displayIdOfNumberAndOperator.childNodes.nodeValue
-// Baris akhir variable
-
 // Function untuk menampilkan angka
 function numberButtonActived()
-{
+{	
+	var saveNumberValue
 	for(let idButton of numberIdButton) 
-	{
-		let ClickIdNumber = idButton.addEventListener("click",
-			function Number() 
-			{
-				let displayingNumber = displayIdOfNumberAndOperator.append(innerText = idButton.value)
-				for(let arithmeticId of operationId) 
-				{
-					arithmeticId.disabled = false
-				}
-			}, false)
+	{	
+		// console.log(saveNumberValue)
+		let ClickIdNumber = idButton.addEventListener("click",() =>
+		{
+			let displayingNumber = displayIdOfNumberAndOperator.append(innerText = idButton.value)
+			for(let arithmeticId of operationId) 
+			{	
+				arithmeticId.disabled = false
+				// console.log(saveNumberValue)
+			}
+		}, false)
+		return saveNumberValue = idButton.value
 	}
 }
-console.log(numberButtonActived() + [numberIdButton.value])
+console.log(numberButtonActived())
 // Baris akhir function 
 
 // Baris pengkondisian untuk menampilkan operasi matematika
@@ -53,54 +52,146 @@ function operationButtonActived()
 }
 // Baris akhir function
 
-function EnabledCondition()
-{
-	let equalButtonActiveCondition = equalButton.disbled += true
-	return equalButtonActiveCondition
-}
+// Function untuk memeriksa tombol angka
+// function EnabledNumberButtonCondition()
+// {
+// 	let numberButton = [numberButtonActived()]
+	
+// 	for(let numberButtonTrue of numberButton) 
+// 	{
+// 		numberButtonTrue
+// 	}
+// }
+// Baris akhir function
 
-// Listener untuk menampilkan simbol sama dengan 
-function equalButtonClicked()
-{	
-	let newValue = new Array([numberButtonActived(),operationButtonActived()])
-	let equalButtonInactiveCondition = equalButton.disbled += false
-	for(let enabledValue of newValue)
-	{	
-		for(let comparedValue = 0; enabledValue.value > comparedValue.value; enabledValue++)
-		{	
-			let showNumberAndOperator = getDomElement[enabledValue]
-			if(displayIdOfNumberAndOperator.style.display = "flex" && showNumberAndOperator.length == 1 || showNumberAndOperator.length > 1)
-			{
-				EnabledCondition()
-			}
-		}
+// Function untuk memeriksa tombol operator
+function EnabledOperationButtonCondition()
+{
+	let equalButton = [operationButtonActived()]
+	
+	for(let operationButtonTrue of equalButton) 
+	{
+		operationButtonTrue
 	}
 }
+// Baris akhir function
+
+// function RunEqualButton() 
+// {	
+// 	const saveEnabledNumberButtonCondition = EnabledNumberButtonCondition()
+// 	if(saveEnabledNumberButtonCondition || saveEnabledNumberButtonCondition != undefined)
+// 	{
+// 		for(let nullValue = 0; saveEnabledNumberButtonCondition > nullValue.length; nullValue++)
+// 		{	
+// 			let numberValueHasBenShows = displayIdOfNumberAndOperator.childNodes[nullValue].nodeValue
+// 			if(numberValueHasBenShows != null || numberValueHasBenShows !== "")
+// 			{
+// 				if(numberValueHasBenShows > 1 || numberValueHasBenShows == 1)
+// 				{
+// 					let equalButtonCanctive = equalButton.disabled += true
+// 					return equalButtonCanctive
+// 				}
+// 				else if(numberValueHasBenShows < 1 || numberValueHasBenShows != 1)
+// 				{
+// 					let equalButtonInactive = equalButton.disabled += false
+// 					return equalButtonInactive
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+// RunEqualButton() 
+
+// Listener untuk menampilkan simbol sama dengan 
+// function EqualButtonClicked()
+// {	
+// 	let newValue = [numberButtonActived(), operationButtonActived()]
+// 	let equalButtonInactiveCondition = equalButton.disbled += false
+// 	for(let enabledValue of newValue)
+// 	{	
+// 		if(enabledValue)
+// 		{	
+// 			for(let comparedValue = 0; enabledValue.value > comparedValue.value; enabledValue++)
+// 			{	
+// 				let showNumberAndOperator = displayIdOfNumberAndOperator.childNodes[enabledValue].nodeValue
+// 				if(showNumberAndOperator)
+// 				{
+// 					if(showNumberAndOperator != null || showNumberAndOperator !== "")
+// 					{
+// 						if(displayIdOfNumberAndOperator.style.display = "flex" && showNumberAndOperator.innerText.length == 1 || showNumberAndOperator.innerText.length > 1)
+// 						{
+// 							let equalButtonActiveCondition = equalButton.disbled += true
+// 							return equalButtonActiveCondition 
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 // Baris akhir function 
 
 // Function untuk membuat tombol sama dengan aktiv
-function enabledEachequalButtonClicked()
+// function EnabledEachequalButtonClicked()
+// {	
+// 	switch(RunEqualButton())	
+// 	{
+// 		case true : 
+// 					equalButton.addEventListener("click", () =>
+// 					{	 
+// 						console.log("Button Enabled")
+// 						displayIdOfNumberAndOperator.style.display = "none"
+// 					},{once:true})
+// 		break
+// 	}
+// }
+// EnabledEachequalButtonClicked()
+// Baris akhir function
+
+// Funciton untuk uji coba
+function testEqualButton()
 {	
-	switch(equalButtonClicked())	
+	let variable = displayIdOfNumberAndOperator.childNodes += numberButtonActived(window.idButton)
+	let buttonTrue = equalButton.disabled = true
+	switch(variable)
 	{
-		case true : 
-					window.equalButtonActiveCondition.addEventListener("click", () =>
-					{	 
-						displayIdOfNumber.style.display = "none"
-					},{once:true})
+		case variable : 
+		case variable.length > 1 : console.log(variable)
 		break
 	}
 }
-enabledEachequalButtonClicked()
+let runEqualButton = testEqualButton()
+
+if(runEqualButton)
+{
+	window.buttonTrue+equalButton.addEventListener("click", () =>
+	{	 
+		console.log("Button Enabled")
+		displayIdOfNumberAndOperator.style.display = "none"
+	},{once:true})
+}
+
+function Operators()
+{
+	let mathOperators = ["+", "-", "/", "*"]
+
+	let savemathOperators
+	for(let itterateMathOperators of mathOperators)
+	{	
+		console.log(savemathOperators = itterateMathOperators)
+	}
+}
+Operators()
 // Baris akhir function
+
 
 let runCalculations = new Promise((IdNumberClicked) =>
 {	
 	IdNumberClicked()
 })
-runCalculations.then((equalButtonClicked) =>
+runCalculations.then((EnabledEachequalButtonClicked) =>
 {
-	switch(window.arithmeticId)
+	switch(operationButtonActived)
 	{	
 		case "+" : let sumValue = showFinalResult.innerText = Number(numberIdButton.value) + Number(numberIdButton.value)
 		break
